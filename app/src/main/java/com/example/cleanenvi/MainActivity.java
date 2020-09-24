bb2a2d4c56ccbd08beb41f2dcedf49b2732cb099
Malegr0 <schoening.andreas@gmx.de>
2020-09-24 20:22:59 +0200

    noch etwas eingefügt

--- app/src/main/java/com/example/cleanenvi/MainActivity.java
+++ app/src/main/java/com/example/cleanenvi/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test
+        //und hier teste ich noch was
     }
 }
