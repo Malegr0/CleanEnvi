cd67417bf93c4fcfd4081ea955a728ef670ec1b7
KaranIses <71843219+KaranIses@users.noreply.github.com>
2020-10-03 14:29:56 +0200

    Testen des product_search branch mit Kommentar in MainActivity

--- app/src/main/java/com/example/cleanenvi/MainActivity.java
+++ app/src/main/java/com/example/cleanenvi/MainActivity.java
@@ -10,8 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //wieder normal
-
         //dies sind Änderungen im product_search branch
+        //wieder normal
     }
 }
