8c2992bea2244f615e0b963c648e1e5731e89a27
bwxt856 <bwxt856@bwb.de>
2020-10-15 11:32:43 +0200

    Safety Commit.

--- app/src/main/java/com/example/cleanenvi/MainActivity.java
+++ app/src/main/java/com/example/cleanenvi/MainActivity.java
@@ -9,8 +9,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //wieder normal
-        //das hier sind die Änderungen des recycling branches
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test);
+       //java.lang.System.out.println(findViewById(R.id.test_button).text);
+
     }
 }
