91fa0685dd6262e5d853a7e81823f559a76fa45f
csteuer-beep <chrsteuer31@gmail.com>
2020-10-15 12:40:15 +0200

    Produkt Class angelegt und berabeitet

---
+++ app/src/main/java/com/example/cleanenvi/Produkt.java
@@ -0,0 +1,29 @@
+package com.example.cleanenvi;
+
+public class Produkt {
+    //fields
+    private String prodname;
+    private int entnum;
+
+    //constructors
+    public Produkt(){}
+    public Produkt(String id, int entnum) {
+        this.prodname = id;
+        this.entnum = entnum;
+    }
+    //properties
+    public void setName(String id) {
+        this.prodname = id;
+    }
+    public String getName(){
+        return this.prodname;
+    }
+
+    public void setentnum(int entnum) {
+        this.entnum= entnum;
+    }
+    public int getentnum () {
+        return this.entnum;
+    }
+
+}
