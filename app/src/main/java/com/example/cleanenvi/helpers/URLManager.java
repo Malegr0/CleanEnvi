d391a49ca61731eaaf6bf6159f0175038ad514aa
Malegr0 <schoening.andreas@gmx.de>
2021-02-23 00:01:58 +0100

    added get method for newsfeed

--- app/src/main/java/com/example/cleanenvi/helpers/URLManager.java
+++ app/src/main/java/com/example/cleanenvi/helpers/URLManager.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class URLManager {
@@ -11,7 +12,7 @@ public class URLManager {
     private static final String  SERVER_ADDRESS = "http://malegro.ddns.net:8080";
     private static final String  PRODUCTS_ADDRESS = SERVER_ADDRESS + "/products/";
     private static final String  PACKAGES_ADDRESS = SERVER_ADDRESS + "/packages/";
-    //private static final String  NEWSFEED_ADDRESS = SERVER_ADDRESS + "/newsfeed/";
+    private static final String  NEWSFEED_ADDRESS = SERVER_ADDRESS + "/newsfeed/";
 
     public static String getRecID(String packaging) throws IOException {
         URL url = new URL(PACKAGES_ADDRESS + packaging);
@@ -32,6 +33,27 @@ public class URLManager {
 
     public static String getProduct(String ean) throws IOException {
         URL url = new URL(PRODUCTS_ADDRESS + ean);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        String response = "";
+        if(conn.getResponseCode() == 200) {
+            BufferedReader bf = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            while((inputLine = bf.readLine()) != null) {
+                response = response + inputLine;
+            }
+            bf.close();
+        } else if(conn.getResponseCode() == 404) {
+            response = null;
+        }
+        conn.disconnect();
+        return response;
+    }
+
+    //TODO: check response code handling
+    //TODO: add method for getting current number of newsfeed database rows
+    public static String getNewsfeed(int id) throws IOException {
+        URL url = new URL(NEWSFEED_ADDRESS + id);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         String response = "";
