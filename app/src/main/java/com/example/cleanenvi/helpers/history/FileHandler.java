7def72c77f3aa7117d612c65b4dce9d0bd419dbe
Malegr0 <schoening.andreas@gmx.de>
2021-03-07 20:58:47 +0100

    added GSON dependency, FileHandler methods now using gson to save data

--- app/src/main/java/com/example/cleanenvi/helpers/history/FileHandler.java
+++ app/src/main/java/com/example/cleanenvi/helpers/history/FileHandler.java
@@ -2,9 +2,14 @@ package com.example.cleanenvi.helpers.history;
 
 import android.content.Context;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
@@ -13,18 +18,25 @@ public class FileHandler {
     //saves Date to file, if file does not exist, it creates the file
     public static void saveData(Context context, History[] histories) throws IOException {
         FileOutputStream outputStream = context.openFileOutput("history", Context.MODE_PRIVATE);
-        ObjectOutputStream out = new ObjectOutputStream(outputStream);
-        out.writeObject(histories);
-        out.close();
+        Gson gson = new Gson();
+        String s = gson.toJson(histories);
+        outputStream.write(s.getBytes());
+        outputStream.close();
     }
 
     //opens file and reads data
     public static History[] loadData(Context context) throws IOException, ClassNotFoundException {
         FileInputStream inputStream = context.openFileInput("history");
-        ObjectInputStream in = new ObjectInputStream(inputStream);
-        History[] histories = (History[]) in.readObject();
-        in.close();
-        return histories;
+        InputStreamReader isr = new InputStreamReader(inputStream);
+        BufferedReader br = new BufferedReader(isr);
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+        String json = sb.toString();
+        Gson gson = new Gson();
+        return gson.fromJson(json, History[].class);
     }
 
 }
