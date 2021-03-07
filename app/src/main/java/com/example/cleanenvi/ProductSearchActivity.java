5cc34df0689e42a3466fb199665d40a3cb1790d2
Malegr0 <schoening.andreas@gmx.de>
2021-03-07 16:49:05 +0100

    added textviews and renamed variables for naming conventions

--- app/src/main/java/com/example/cleanenvi/ProductSearchActivity.java
+++ app/src/main/java/com/example/cleanenvi/ProductSearchActivity.java
@@ -10,18 +10,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.cleanenvi.helpers.history.History;
+import com.example.cleanenvi.helpers.history.HistoryManager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 public final class ProductSearchActivity extends AppCompatActivity {
     EditText productSearchEdit;
     public static String EAN;
     ImageButton searchBtn;
-    ImageView HisPro1, HisPro2, HisPro3, HisPro4, HisPro5;
+    ImageView hisPro1, hisPro2, hisPro3, hisPro4, hisPro5;
+    TextView hisProText1, hisProText2, hisProText3, hisProText4, hisProText5;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,23 +35,34 @@ public final class ProductSearchActivity extends AppCompatActivity {
         this.setTitle("Produktsuche");
         productSearchEdit =  findViewById(R.id.productSearchEdit);
         searchBtn = findViewById(R.id.searchbtn);
-        HisPro1 = findViewById(R.id.historyImage1);
-        HisPro2 = findViewById(R.id.historyImage2);
-        HisPro3 = findViewById(R.id.historyImage3);
-        HisPro4 = findViewById(R.id.historyImage4);
-        HisPro5 = findViewById(R.id.historyImage5);
+        hisPro1 = findViewById(R.id.historyImage1);
+        hisProText1 = findViewById(R.id.historyText1);
+        hisPro2 = findViewById(R.id.historyImage2);
+        hisProText2 = findViewById(R.id.historyText2);
+        hisPro3 = findViewById(R.id.historyImage3);
+        hisProText3 = findViewById(R.id.historyText3);
+        hisPro4 = findViewById(R.id.historyImage4);
+        hisProText4 = findViewById(R.id.historyText4);
+        hisPro5 = findViewById(R.id.historyImage5);
+        hisProText5 = findViewById(R.id.historyText5);
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_search);
 
         watcher(productSearchEdit, searchBtn);
 
         //History loading
+        try {
+            HistoryManager.loadHistories(this);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
 
         // Nutella https://www.testberichte.de/imgs/p_imgs_370/11/11709.jpg
-        Picasso.get().load("https://www.testberichte.de/imgs/p_imgs_370/11/11709.jpg").into(HisPro1);
-        Picasso.get().load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSebbILIsbkl0u52kWQxmBofyJcYhoCWq5_qA&usqp=CAU").into(HisPro2);
-        Picasso.get().load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3Md53AHlyPg5COqwMdgQsDjuVX3ScetOucg&usqp=CAU").into(HisPro3);
-        Picasso.get().load("https://www.testberichte.de/imgs/p_imgs_370/11/11709.jpg").into(HisPro4);
-        Picasso.get().load("https://www.testberichte.de/imgs/p_imgs_370/11/11709.jpg").into(HisPro5);
+        Picasso.get().load("https://www.testberichte.de/imgs/p_imgs_370/11/11709.jpg").into(hisPro1);
+        Picasso.get().load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSebbILIsbkl0u52kWQxmBofyJcYhoCWq5_qA&usqp=CAU").into(hisPro2);
+        Picasso.get().load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3Md53AHlyPg5COqwMdgQsDjuVX3ScetOucg&usqp=CAU").into(hisPro3);
+        Picasso.get().load("https://www.testberichte.de/imgs/p_imgs_370/11/11709.jpg").into(hisPro4);
+        Picasso.get().load("https://www.testberichte.de/imgs/p_imgs_370/11/11709.jpg").into(hisPro5);
 
         searchBtn.setOnClickListener(new View.OnClickListener() {
             public final void onClick(View it) {
@@ -69,6 +86,12 @@ public final class ProductSearchActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    void loadHistoriesIntoLayoutFields(History[] histories) {
+        if(histories[0].getEan() != null) {
+
+        }
     }
 
     //Aktiviert Button bei Texteingabe und speichert Eingabe für spätere Verarbeitung
